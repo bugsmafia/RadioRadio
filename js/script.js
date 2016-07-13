1b788cee0a83c8e18be7c6f33952ed0db0cdc7b0
Сергей <bugsmafia@gmail.com>
2016-07-13 09:01:34 +0500

    1

--- js/script.js
+++ js/script.js
@@ -118,6 +118,8 @@ function onError(e){
 
 var url = 'http://play.radio13.ru/aac';
 var streamer = 1;
+function StInit(){
+	alert('загрузка конфигураций');
 var my_media = new PlayStream(url,
     function (status){
 		console.log(status); 
@@ -138,10 +140,12 @@ var my_media = new PlayStream(url,
         alert(err);
     }
 );
+}
 
 function streamplay() {
 	alert('Нажали плей');
 	if (streamer == "1") {
+		StInit();
 		alert('включаем 1 поток');
 		//player.Play('http://play.radio13.ru/mp3');
 		my_media.play();
