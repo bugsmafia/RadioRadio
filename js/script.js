71306939f90c09eb1c17e8e955a8146759a85e43
Сергей <bugsmafia@gmail.com>
2016-09-24 12:02:05 +0500

    1

--- js/script.js
+++ js/script.js
@@ -5,9 +5,7 @@ function getPageName(url) {
 }
 
 // Функция выполнения кода при загрузки приложения
-function onLoad() {
-	
-    
+function onLoad() {    
 }
 // Функция исполнения когда приложение готово
 function onDeviceReady() {
@@ -132,271 +130,16 @@ function Config(data) {
 	}
 }
 
-// Тянем информацию об альбоме
-function infoAlbum(type, id, md, artist, song) {
-    var api = '88571316d4e244f24172ea9a9bf602fe';
-    jQuery.ajax({
-        url: "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&artist=" + artist + "&album=" + song + "&api_key=" + api,
-        type: "GET",
-        dataType: "xml",
-        success: function(xml) {
-            jQuery(xml).find('album').each(function() {
-                jQuery(this).find('image').each(function() {
-                    var img = jQuery(this).attr('size');
-                    if (img == "small") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
-                            localStorage.setItem(md + 'Small', $himg);
-                        }
-                    };
-                    if (img == "medium") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
-                            localStorage.setItem(md + 'Medium', $himg);
-                        }
-                    };
-                    if (img == "large") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
-                            localStorage.setItem(md + 'Large', $himg);
-                        }
-                    };
-                    if (img == "extralarge") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
 
-                            localStorage.setItem(md + 'Extralarge', $himg);
-                        }
-                    }
-                    if (img == "mega") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
-                            localStorage.setItem(md + 'Mega', $himg);
-                        } else {
-                            infoArtist(type, id, md, artist, song);
-                        }
-                    }
-                })
-            });
 
-            var ImgCashSmall = ContentSync.sync({
-                src: localStorage.TrackIdNowImgSmall,
-                id: 'small/' + getPageName(localStorage.TrackIdNowImgSmall),
-                type: 'local'
-            });
-            var ImgCashMedium = ContentSync.sync({
-                src: localStorage.TrackIdNowImgMedium,
-                id: 'medium/' + getPageName(localStorage.TrackIdNowImgMedium),
-                type: 'local'
-            });
-            var ImgCashLarge = ContentSync.sync({
-                src: localStorage.TrackIdNowImgLarge,
-                id: 'large/' + getPageName(localStorage.TrackIdNowImgLarge),
-                type: 'local'
-            });
-            var ImgCashExtralarge = ContentSync.sync({
-                src: localStorage.TrackIdNowImgExtralarge,
-                id: 'extralarge/' + getPageName(localStorage.TrackIdNowImgExtralarge),
-                type: 'local'
-            });
-            var ImgCashMega = ContentSync.sync({
-                src: localStorage.TrackIdNowImgMega,
-                id: 'mega/' + getPageName(localStorage.TrackIdNowImgMega),
-                type: 'local'
-            });
 
-            ImgCashSmall.on('complete', function(data) {
-                console.log(data.localPath);
-                console.log(data.cached);
-            });
-            ImgCashMedium.on('complete', function(data) {
-                console.log(data.localPath);
-                console.log(data.cached);
-            });
-            ImgCashLarge.on('complete', function(data) {
-				console.log('Ларге - отправим в бар');
-                console.log(data.localPath);
-                console.log(data.cached);
-				ContentSync.loadUrl('file://' + data.localPath, function() {
-					if (data.localPath) {
-						statusBar(data.localPath);
-						console.log('Ларге - отправили в бар Готово');
-					}
-				});
-                
-            });
-            ImgCashExtralarge.on('complete', function(data) {
-                console.log(data.localPath);
-                console.log(data.cached);
-                if (data.localPath) {
-					console.log(data.localPath);
-                }
-
-            });
-            ImgCashMega.on('complete', function(data) {
-                console.log(data.localPath);
-                console.log(data.cached);
-            });
-
-        },
-        statusCode: {
-            400: function() {
-                infoArtist(type, id, md, artist, song);
-				statusBar('icon.png');
-            }
-        }
-    });
-}
-// Тянем информацию об артисте 
-function infoArtist(type, id, md, artist, song) {
-    var api = '88571316d4e244f24172ea9a9bf602fe';
-    jQuery.ajax({
-        url: "http://ws.audioscrobbler.com/2.0/?method=artist.getInfo&artist=" + artist + "&api_key=" + api,
-        type: "GET",
-        dataType: "xml",
-        success: function(xml) {
-            jQuery(xml).find('artist').each(function() {
-                jQuery(this).find('image').each(function() {
-                    var img = jQuery(this).attr('size');
-                    if (img == "small") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
-                            localStorage.setItem(md + 'Small', $himg);
-                        }
-                    };
-                    if (img == "medium") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
-                            localStorage.setItem(md + 'Medium', $himg);
-                        }
-                    };
-                    if (img == "large") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
-                            localStorage.setItem(md + 'Large', $himg);
-                        };
-                    };
-                    if (img == "extralarge") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
-                            localStorage.setItem(md + 'Extralarge', $himg);
-                        }
-                    }
-                    if (img == "mega") {
-                        if (jQuery(this).text()) {
-                            $himg = jQuery(this).text();
-                            localStorage.setItem(md + 'Mega', $himg);
-                        }
-                    }
-                })
-            });
-
-            var ImgCashSmall = ContentSync.sync({
-                src: localStorage.TrackIdNowImgSmall,
-                id: 'small/' + getPageName(localStorage.TrackIdNowImgSmall),
-                type: 'local'
-            });
-            var ImgCashMedium = ContentSync.sync({
-                src: localStorage.TrackIdNowImgMedium,
-                id: 'medium/' + getPageName(localStorage.TrackIdNowImgMedium),
-                type: 'local'
-            });
-            var ImgCashLarge = ContentSync.sync({
-                src: localStorage.TrackIdNowImgLarge,
-                id: 'large/' + getPageName(localStorage.TrackIdNowImgLarge),
-                type: 'local'
-            });
-            var ImgCashExtralarge = ContentSync.sync({
-                src: localStorage.TrackIdNowImgExtralarge,
-                id: 'extralarge/' + getPageName(localStorage.TrackIdNowImgExtralarge),
-                type: 'local'
-            });
-            var ImgCashMega = ContentSync.sync({
-                src: localStorage.TrackIdNowImgMega,
-                id: 'mega/' + getPageName(localStorage.TrackIdNowImgMega),
-                type: 'local'
-            });
-
-            ImgCashSmall.on('complete', function(data) {
-                console.log(data.localPath);
-                console.log(data.cached);
-            });
-            ImgCashMedium.on('complete', function(data) {
-                console.log(data.localPath);
-                console.log(data.cached);
-
-            });
-            ImgCashLarge.on('complete', function(data) {
-				console.log('Ларге - отправим в бар');
-                console.log(data.localPath);
-                console.log(data.cached);
-				ContentSync.loadUrl('file://' + data.localPath, function() {
-					if (data.localPath) {
-						statusBar(data.localPath);
-						console.log('Ларге - отправили в бар Готово');
-					}
-				});
-                
-            });
-            ImgCashExtralarge.on('complete', function(data) {
-                console.log(data.localPath);
-                console.log(data.cached);
-            });
-            ImgCashMega.on('complete', function(data) {
-                console.log(data.localPath);
-                console.log(data.cached);
-            });
-        },
-        statusCode: {
-            400: function() {
-				statusBar('icon.png'); 				
-			}
-        }
-    });
-}
-
-// Загружаем статус эфира
-function LoadStatus() {
-    jQuery.getJSON("http://app.radioradio.ru/json.php?i=i", function(data) {
-        UpdateStatus(data.i);
-    });
-}
 
 
 // Устанавливаем первоначальное значение куки о треке
 localStorage.setItem('TrackIdNow', '');
 
-// Каждые 15 секунд запрашиваем статус эфира
-setInterval(function() {
-    LoadStatus();
-    $('#trace').html(window.location.pathname + ' ' + localStorage.TrackIdNow);
-}, 15000);
-// Обновляем статус эфира
-function UpdateStatus(now) {
-    if (localStorage.TrackIdNow == now) {} else {
-        jQuery.getJSON("http://app.radioradio.ru/json.php?i=l", function(data) {
-            setTimeout(function() {
-                jQuery("#playinfo").addClass("show");
-                // название трека
-                jQuery('.songinfo').text(data.s);
-                jQuery('.song').text(data.s);
-                // артист
-                jQuery('.titleinfo').text(data.a);
-                jQuery('.artist').text(data.a);
-                // Обновляет куки
-                localStorage.setItem('NowSong', data.s);
-                localStorage.setItem('NowArtist', data.a);
-            }, 2000);
-            localStorage.setItem('TrackIdNow', data.id);
-            if (localStorage.getItem('ConfloadAlbum') == 'false') {
-				  infoAlbum('playinfo', 'playinfoimg', 'TrackIdNowImg', data.a, data.s);
-            } else {               
-				console.log('Загрузка изображений альбома отключена.');
-                statusBar('icon.png');        
-            };
-        });
-    } 
-}
+
+
 
 // Статус, играет или нет.
 var Playing = false;
@@ -773,8 +516,6 @@ ons.ready(function() {
 	function volumecurrent(){
 		window.plugins.mediaVolume.getVol(
 		  function(data){
-			//data.current 当前音量
-			//data.max 系统最大音量
 			var cur = Math.floor((data.current * 100) / data.max);
 			jQuery("#volume").val(cur)
 		  },
