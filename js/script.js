326f2bc9b7c5d67b3ac90be78fd56b6136f57d3a
Сергей <bugsmafia@gmail.com>
2016-10-07 09:00:05 +0500

    ru.es.radio

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
 document.addEventListener('deviceready', function () {
-    // Android customization
-    //cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
-    // Enable background mode
     cordova.plugins.backgroundMode.enable();
-
-    // Called when background mode has been activated
-    cordova.plugins.backgroundMode.onactivate = function () {
-        setTimeout(function () {
-            // Modify the currently displayed notification
-            //cordova.plugins.backgroundMode.configure({
-            //    text:'Running in background for more than 5s now.'
-            //});
-        }, 5000);
-    } 
 }, false);
 
 function Loader() {
